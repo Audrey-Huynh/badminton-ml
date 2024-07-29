4194ddc7b56213f56974ee4992726ab65938f36f
Audrey-Huynh <86124524+Audrey-Huynh@users.noreply.github.com>
2024-07-29 16:46:11 -0700

    added scripts

---
+++ Assets/MyScripts/BadmintonSettings.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadmintonSettings : MonoBehaviour
+{
+    public float agentRunSpeed = 1.5f;
+    public float agentJumpHeight = 2.75f;
+    public float agentJumpVelocity = 777;
+    public float agentJumpVelocityMaxChange = 10;
+
+    // Slows down strafe & backward movement
+    public float speedReductionFactor = 0.75f;
+
+    public Material blueGoalMaterial;
+    public Material purpleGoalMaterial;
+    public Material defaultMaterial;
+
+    // This is a downward force applied when falling to make jumps look less floaty
+    public float fallingForce = 150;
+}
