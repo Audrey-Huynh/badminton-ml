59189c2f6c4bb3bfb4100a4166d94de54e6731d0
Audrey-Huynh <86124524+Audrey-Huynh@users.noreply.github.com>
2024-07-31 12:57:02 -0700

    added scripts

---
+++ Assets/MyScripts/BirdieController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdieController : MonoBehaviour
+{
+    [HideInInspector]
+    public BirdieEnvController envController;
+
+    public GameObject PurpleGoal;
+    public GameObject BlueGoal;
+    Collider purpleGoalCollider;
+    Collider blueGoalCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        envController = GetComponentInParent<BirdieEnvController>();
+        purpleGoalCollider = PurpleGoal.GetComponent<Collider>();
+        blueGoalCollider = BlueGoal.GetComponent<Collider>();
+    }
+
+    /// <summary>
+    /// Detects whether the birdie lands in the blue, purple, or out of bounds area
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("boundary"))
+        {
+            // birdie went out of bounds
+            envController.ResolveEvent(Event.HitOutOfBounds);
+        }
+        else if (other.gameObject.CompareTag("blueBoundary"))
+        {
+            // birdie hit into blue side
+            envController.ResolveEvent(Event.HitIntoBlueArea);
+        }
+        else if (other.gameObject.CompareTag("purpleBoundary"))
+        {
+            // birdie hit into purple side
+            envController.ResolveEvent(Event.HitIntoPurpleArea);
+        }
+        else if (other.gameObject.CompareTag("purpleGoal"))
+        {
+            // birdie hit purple goal (blue side court)
+            envController.ResolveEvent(Event.HitPurpleGoal);
+        }
+        else if (other.gameObject.CompareTag("blueGoal"))
+        {
+            // birdie hit blue goal (purple side court)
+            envController.ResolveEvent(Event.HitBlueGoal);
+        }
+    }
+
+
+}
